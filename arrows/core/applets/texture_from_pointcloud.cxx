214ef100d63c8d10f534ac6921cdfafef2b04afe
Chet Nieter <chet.nieter@gmail.com>
2022-06-30 18:12:10 -0400

    Added methods for configuration and initialization
    Added methods to parse command line arguments, set and
    check configuration for validity, and initialize the
    applet.

--- arrows/core/applets/texture_from_pointcloud.cxx
+++ arrows/core/applets/texture_from_pointcloud.cxx
@@ -12,6 +12,7 @@
 #include <iostream>
 
 #include <vital/algo/nearest_neighbors.h>
+#include <vital/applets/config_validation.h>
 #include <vital/config/config_block.h>
 #include <vital/config/config_block_formatter.h>
 #include <vital/config/config_block_io.h>
@@ -33,21 +34,32 @@ std::string opt_out_config;     // output config file name
 int opt_width;
 int opt_height;
 
-// ------------------------------------------------------------------
-static kwiver::vital::config_block_sptr
-default_config()
+kwiver::vital::logger_handle_t main_logger( kwiver::vital::get_logger(
+                                              "texture_from_pointcloud" ) );
+
+// ----------------------------------------------------------------------------
+bool
+check_config( kwiver::vital::config_block_sptr config )
 {
-  kwiver::vital::config_block_sptr config =
-    kwiver::vital::config_block::empty_config( "texture-from-pointcloud-tool" );
+  using namespace kwiver::tools;
 
-  config->set_value( "nearest_neighbors:type", "vxl_kd_tree",
-                     "Implementation for nearest neighbor search." );
+  bool config_valid = true;
 
-  kwiver::vital::algo::nearest_neighbors::get_nested_algo_configuration(
-    "nearest_neighbors", config,
-    kwiver::vital::algo::nearest_neighbors_sptr() );
+#define KWIVER_CONFIG_FAIL( msg ) \
+  LOG_ERROR( main_logger, "Config Check Fail: " << msg ); \
+  config_valid = false
 
-  return config;
+  config_valid =
+    validate_required_input_dir( "mesh_directory", *config,  main_logger ) &&
+    config_valid;
+
+  config_valid =
+    validate_required_input_file( "point-cloud-file", *config, main_logger ) &&
+    config_valid;
+
+#undef KWIVER_CONFIG_FAIL
+
+  return config_valid;
 }
 } // end namespace
 
@@ -56,6 +68,116 @@ class texture_from_pointcloud::priv
 public:
 
   priv() {}
+
+  kwiver::vital::config_block_sptr config;
+  kwiver::vital::algo::nearest_neighbors_sptr nn_search;
+
+  kwiver::vital::path_t mesh_directory;
+  kwiver::vital::path_t point_cloud_file;
+
+  enum commandline_mode { SUCCESS, HELP, WRITE, FAIL };
+
+  commandline_mode
+  process_command_line( cxxopts::ParseResult& cmd_args )
+  {
+    static std::string opt_config;
+    static std::string opt_out_config;
+
+    if( cmd_args[ "help" ].as< bool >() )
+    {
+      return HELP;
+    }
+    if( cmd_args.count( "config" ) > 0 )
+    {
+      opt_config = cmd_args[ "config" ].as< std::string >();
+    }
+    if( cmd_args.count( "output-config" ) > 0 )
+    {
+      opt_out_config = cmd_args[ "output-config" ].as< std::string >();
+    }
+
+    // Set up top level configuration w/ defaults where applicable.
+    config = default_config();
+
+    // If -c/--config given, read in confg file, merge in with default just
+    // generated
+    if( !opt_config.empty() )
+    {
+      config->merge_config( kwiver::vital::read_config_file( opt_config ) );
+    }
+
+    if( cmd_args.count( "mesh-dir" ) > 0 )
+    {
+      mesh_directory = cmd_args[ "mesh-dir" ].as< std::string >();
+      config->set_value( "mesh_directory", mesh_directory );
+    }
+    if( cmd_args.count( "point-cloud-file" ) > 0 )
+    {
+      point_cloud_file = cmd_args[ "point-cloud-file" ].as< std::string >();
+      config->set_value( "point_cloud_file", point_cloud_file );
+    }
+
+    bool valid_config = check_config( config );
+
+    if( !opt_out_config.empty() )
+    {
+      write_config_file( config, opt_out_config );
+      if( valid_config )
+      {
+        LOG_INFO( main_logger,
+                  "Configuration file contained valid parameters and may be "
+                  "used for running" );
+      }
+      else
+      {
+        LOG_WARN( main_logger, "Configuration deemed not valid." );
+      }
+      config = nullptr;
+      return WRITE;
+    }
+    else if( valid_config )
+    {
+      LOG_ERROR( main_logger, "Configuration not valid." );
+      config = nullptr;
+      return FAIL;
+    }
+
+    return SUCCESS;
+  }
+
+  // ------------------------------------------------------------------
+
+  static kwiver::vital::config_block_sptr
+  default_config()
+  {
+    kwiver::vital::config_block_sptr config =
+      kwiver::vital::config_block::empty_config( "texture-from-pointcloud-tool" );
+
+    config->set_value( "nearest_neighbors:type", "vxl_kd_tree",
+                       "Implementation for nearest neighbor search." );
+
+    kwiver::vital::algo::nearest_neighbors::get_nested_algo_configuration(
+      "nearest_neighbors", config,
+      kwiver::vital::algo::nearest_neighbors_sptr() );
+
+    return config;
+  }
+
+  void
+  initialize()
+  {
+    config = this->default_config();
+
+    kwiver::vital::algo::nearest_neighbors::set_nested_algo_configuration(
+      "nearest_neighbors", config, nn_search );
+    kwiver::vital::algo::nearest_neighbors::get_nested_algo_configuration(
+      "nearest_neighbors", config, nn_search );
+  }
+
+  void
+  run_algorithm()
+  {
+  }
 };
 
 // ----------------------------------------------------------------------------
@@ -73,18 +195,21 @@ texture_from_pointcloud
   m_cmd_options->positional_help( "\n mesh-dir         - directory that holds "
                                   "the mesh files."
                                   "\n point-cloud-file - the file that contains "
-                                  "the point cloud data.");
+                                  "the point cloud data." );
 
-  m_cmd_options->add_options()
-    ( "h,help",        "Display usage information" )
-    ( "c",             "Configuration file for tool" )
-    ( "output-config", "Dump configuration for tool", cxxopts::value<std::string>() )
+  m_cmd_options->add_options()( "h,help",
+                                "Display usage information" )( "c",
+                                                               "Configuration file for tool" )(
+    "output-config",
+    "Dump configuration for tool",
+    cxxopts::value< std::string >() )
+  // positional parameters
+    ( "mesh-dir",          "Mesh directory",
+    cxxopts::value< std::string >() )( "point-cloud-file",
+                                       "Point cloud file name",
+                                       cxxopts::value< std::string >() );
 
-    // positional parameters
-    ( "mesh-dir",          "Mesh directory", cxxopts::value<std::string>() )
-    ( "point-cloud-file",  "Point cloud file name", cxxopts::value<std::string>() );
-
-    m_cmd_options->parse_positional({"mesh-dir", "point-cloud-file"});
+  m_cmd_options->parse_positional( { "mesh-dir", "point-cloud-file" } );
 }
 
 // ----------------------------------------------------------------
@@ -109,7 +234,7 @@ texture_from_pointcloud
     if( ( cmd_args.count( "mesh-dir" ) == 0 ) ||
         ( cmd_args.count( "point-cloud-file" ) == 0 ) )
     {
-      std::cout << "Missing file name.\n" <<
+      std::cout << "Missing direcotry and/or file name.\n" <<
         "Usage: " << applet_name() <<
         " mesh-dir point-cloud-file\n" <<
         std::endl;
@@ -118,20 +243,13 @@ texture_from_pointcloud
     }
   }
 
-  kwiver::vital::algo::nearest_neighbors_sptr nn_search;
-  kwiver::vital::config_block_sptr config = default_config();
   // If --config given, read in config file, merge in with default just
   // generated
   if( cmd_args.count( "c" ) > 0 )
   {
-    config->merge_config( kwiver::vital::read_config_file(
-                            cmd_args[ "c" ].as< std::string >() ) );
+    d->config->merge_config( kwiver::vital::read_config_file(
+                               cmd_args[ "c" ].as< std::string >() ) );
   }
-
-  kwiver::vital::algo::nearest_neighbors::set_nested_algo_configuration(
-    "nearest_neighbors", config, nn_search );
-  kwiver::vital::algo::nearest_neighbors::get_nested_algo_configuration(
-    "nearest_neighbors", config, nn_search );
 
   // Check to see if we are to dump config
   if( cmd_args.count( "output-config" ) > 0 )
@@ -144,14 +262,14 @@ texture_from_pointcloud
       return EXIT_FAILURE;
     }
 
-    kwiver::vital::config_block_formatter fmt( config );
+    kwiver::vital::config_block_formatter fmt( d->config );
     fmt.print( fout );
     std::cout << "Wrote config to \"" << opt_out_config << "\". Exiting.\n";
     return EXIT_SUCCESS;
   }
 
   if( !kwiver::vital::algo::nearest_neighbors::check_nested_algo_configuration(
-        "nearest_neighbors", config ) )
+        "nearest_neighbors", d->config ) )
   {
     std::cerr << "Invalid nearest_neighbors config" << std::endl;
     return EXIT_FAILURE;
